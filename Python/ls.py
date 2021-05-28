072c6814e13d86a9bcc6a2f66358b7823df6665b
akashkb-a01 <75172396+akashkb-a01@users.noreply.github.com>
2021-05-28 17:54:02 +0530

    Add files via upload

---
+++ Python/ls.py
@@ -0,0 +1,20 @@
+import argparse
+
+import os
+import sys
+
+parser = argparse.ArgumentParser(prog = 'ls' , usage = '%(prog)s [options] path' , description='ls' , epilog = 'Enjoy !!')
+parser.add_argument('Path' , metavar = 'path' , type = str , action = 'store', nargs='?' ,default = os.getcwd() ,help = 'path to ls, current drectory by default')
+parser.add_argument('-l' , '--long' , action = 'store_true' , help = 'long list')
+args = parser.parse_args()
+if os.path.isdir(args.Path):
+	if args.long:
+		for line in os.listdir(args.Path):
+			if args.long :
+				size = os.stat(os.path.join(args.Path , line)).st_size
+				line = '%10d %s' %(size , line)
+				print(line)
+	else:
+		print('\n' .join(os.listdir(args.Path)))
+else:
+	print('The path does not exist')
