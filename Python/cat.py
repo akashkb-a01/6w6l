e408cde1bb841164551618ff9c6925e80fbf24e1
akashkb-a01 <75172396+akashkb-a01@users.noreply.github.com>
2021-05-28 17:51:40 +0530

    Add files via upload

---
+++ Python/cat.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(prog = 'cat' , description = 'Concatenate files to STDOUT' , epilog = 'Enjoy !!')
+parser.add_argument('path' , metavar = 'path' , type = str , help = 'path of file(s)')
+args = parser.parse_args()
+if os.path.isfile(args.path):
+	file = open(args.path, "r")
+	print (file.read())
+else :
+	print('The path to file does not exist')
