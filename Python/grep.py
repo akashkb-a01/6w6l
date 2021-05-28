52baa4557f7207a16d8526e05e9c513a6d261a0c
akashkb-a01 <75172396+akashkb-a01@users.noreply.github.com>
2021-05-28 18:04:15 +0530

    Add files via upload

---
+++ Python/grep.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(prog = 'grep' , description = 'find string in file(s)' , epilog = 'Enjoy !!')
+parser.add_argument('input' , type = str , help = 'Input the string to find')
+parser.add_argument('file' , metavar = 'path' , type = str , help = 'File location to find string')
+args = parser.parse_args()
+if os.path.isfile(args.file):
+	i = args.input
+	f = open(args.file , 'r')
+	for j in f:
+		if i in j:
+			print(j)
+			j = f.readline()
+else :
+	print('File path does not exist')
